4c4ac299da7fd6ca7e0ca79cb8a656b4930a1587
roussetcedric <59645699+roussetcedric@users.noreply.github.com>
2020-05-12 11:55:45 +0200

    Update NetFlix.py

--- NetFlix.py
+++ NetFlix.py
@@ -13,3 +13,8 @@ st.write(x, 'squared is', x * x)
 
 df_Movies = pd.read_csv("https://raw.githubusercontent.com/roussetcedric/WCS/master/imdb_movies_clean_test.csv?token=AOHB6A2PJQGD37K4XBIQ4EK6YEBVM")
 st.dataframe(df_Movies) 
+
+from PIL import Image
+url = df_Movies.iloc[0]["posterURL"]
+image = Image.open(url)
+st.image(image, caption='Sunrise by the mountains',use_column_width=True)
