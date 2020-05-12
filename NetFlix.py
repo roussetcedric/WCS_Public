2606f65581354f195755b7b45aa3ac188c4ec861
roussetcedric <59645699+roussetcedric@users.noreply.github.com>
2020-05-12 11:46:40 +0200

    Update NetFlix.py

--- NetFlix.py
+++ NetFlix.py
@@ -1,5 +1,8 @@
 import streamlit as st
 import pandas as pd
+
+st.title('Test NetFlix')
+
 st.write("""
 # My first app
 Hello *world!*
@@ -7,3 +10,6 @@ Hello *world!*
 
 x = st.slider('x')
 st.write(x, 'squared is', x * x)
+
+df_Movies = pd.read_csv("https://raw.githubusercontent.com/roussetcedric/WCS/master/imdb_movies_clean_test.csv?token=AOHB6A2PJQGD37K4XBIQ4EK6YEBVM")
+st.dataframe(df_Movies) 
