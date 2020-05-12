28cfbefb76171a5150461ddfd1d40b4863774b0d
roussetcedric <59645699+roussetcedric@users.noreply.github.com>
2020-05-12 16:06:55 +0200

    Update NetFlix.py

--- NetFlix.py
+++ NetFlix.py
@@ -4,33 +4,35 @@ from PIL import Image
 import urllib.request
 import io
 
-st.title('Test NetFlix')
-st.write("""
-# My first app
-Hello *world!*
-""")
+df_Movies = pd.read_csv("https://raw.githubusercontent.com/roussetcedric/WCS/master/imdb_movies_clean_test.csv?token=AOHB6A2PJQGD37K4XBIQ4EK6YEBVM")
+st.dataframe(df_Movies) 
+
+def DisplayPoster(UrlToDisplay) :
+  if UrlToDisplay :
+    with urllib.request.urlopen(URL) as url:
+    f = io.BytesIO(url.read())
+    img = Image.open(f)
+    st.image(img, caption=df_Movies.iloc[x-1]["primaryTitle"],width=150)
 
 # Define Side Menu ----------------------------------------------
 st.sidebar.title("Film Survey :movie:")
 movie = st.sidebar.multiselect("Which do you like the most?",
                                 ("Vanilla Yogurt","Berry Yogurt","Greek Yogurt"))
 
+# Define the Main Page
+Titre = "Voici la sélection de film basé sur"
+Param = ""
+st.title(Titre + Param)
+
 x = st.slider('x',1,5)
-
-df_Movies = pd.read_csv("https://raw.githubusercontent.com/roussetcedric/WCS/master/imdb_movies_clean_test.csv?token=AOHB6A2PJQGD37K4XBIQ4EK6YEBVM")
-st.dataframe(df_Movies) 
-
-URL = df_Movies.iloc[x-1]["posterURL"]
-
-with urllib.request.urlopen(URL) as url:
-    f = io.BytesIO(url.read())
-
-img = Image.open(f)
-st.image(img, caption=df_Movies.iloc[x-1]["primaryTitle"],use_column_width=True)
+DisplayPoster(df_Movies.iloc[x-1]["posterURL"])
 
 if st.button('Titre'):
+  Param = df_Movies.iloc[x-1]["primaryTitle"]
   st.write(df_Movies.iloc[x-1]["primaryTitle"])
 if st.button('Genre'):
+  Param = df_Movies.iloc[x-1]["genres"]
   st.write(df_Movies.iloc[x-1]["genres"])
 if st.button('Director'):
+  Param = df_Movies.iloc[x-1]["directorsName"]
   st.write(df_Movies.iloc[x-1]["directorsName"])
