5b72c98c50937b922af6874c1f6d372e9f2f3dd5
roussetcedric <59645699+roussetcedric@users.noreply.github.com>
2020-05-12 12:01:44 +0200

    Update NetFlix.py

--- NetFlix.py
+++ NetFlix.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
 from PIL import Image
-import requests
-from io import BytesIO
+import urllib.request
+import io
 
 st.title('Test NetFlix')
 
@@ -17,8 +17,10 @@ st.write(x, 'squared is', x * x)
 df_Movies = pd.read_csv("https://raw.githubusercontent.com/roussetcedric/WCS/master/imdb_movies_clean_test.csv?token=AOHB6A2PJQGD37K4XBIQ4EK6YEBVM")
 st.dataframe(df_Movies) 
 
+URL = df_Movies.iloc[0]["posterURL"]
 
-url = df_Movies.iloc[0]["posterURL"]
-response = requests.get(url)
-img = Image.open(BytesIO(response.content))
+with urllib.request.urlopen(URL) as url:
+    f = io.BytesIO(url.read())
+
+img = Image.open(f)
 st.image(img, caption='Sunrise by the mountains',use_column_width=True)
