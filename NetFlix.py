a4c91e2db859db02a5202abe5fee9ab41ac0917c
roussetcedric <59645699+roussetcedric@users.noreply.github.com>
2020-05-12 16:48:25 +0200

    Update NetFlix.py

--- NetFlix.py
+++ NetFlix.py
@@ -5,7 +5,9 @@ import urllib.request
 import io
 
 df_Movies = pd.read_csv("https://raw.githubusercontent.com/roussetcedric/WCS/master/imdb_movies_clean_test.csv?token=AOHB6A2PJQGD37K4XBIQ4EK6YEBVM")
-st.dataframe(df_Movies) 
+df_Display = df_Movies.sort_values(by=['primaryTitle'])[0:5]
+Param = "Initilialisation"
+x = 0
 
 def DisplayPoster(UrlToDisplay) :
   if UrlToDisplay :
@@ -22,16 +24,22 @@ Choix = st.sidebar.multiselect("Which do you like the most?",
 # Define the Main Page
 st.write("Voici la sélection de film basée sur")
 st.write(Choix)
+st.write(Param)
 
-x = st.slider('x',1,5)
 DisplayPoster(df_Movies.iloc[x-1]["posterURL"])
+x = st.slider('x',1,5)
+
+st.dataframe(df_Display) 
 
 if st.button('Titre'):
   Param = df_Movies.iloc[x-1]["primaryTitle"]
   st.write(df_Movies.iloc[x-1]["primaryTitle"])
+  df_Display = df_Movies.sort_values(by=['primaryTitle'])[0:5]
 if st.button('Genre'):
   Param = df_Movies.iloc[x-1]["genres"]
   st.write(df_Movies.iloc[x-1]["genres"])
+  df_Display = df_Movies.sort_values(by=['genres'])[0:5]
 if st.button('Director'):
   Param = df_Movies.iloc[x-1]["directorsName"]
   st.write(df_Movies.iloc[x-1]["directorsName"])
+  df_Display = df_Movies.sort_values(by=['directorsName'])[0:5]
