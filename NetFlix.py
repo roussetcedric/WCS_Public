48fd876a060a6f984cbfe58af25dc62b27ac3d92
roussetcedric <59645699+roussetcedric@users.noreply.github.com>
2020-05-14 11:28:30 +0200

    Update NetFlix.py

--- NetFlix.py
+++ NetFlix.py
@@ -41,26 +41,21 @@ my_bar = st.progress(0)
 for percent_complete in range(10):
   time.sleep(0.1)
   my_bar.progress(percent_complete + 10)
-#df_Movies = pd.read_csv("https://raw.githubusercontent.com/roussetcedric/WCS/master/imdb_movies_clean_test.csv?token=AOHB6A2PJQGD37K4XBIQ4EK6YEBVM")
 df_Movies = pd.read_csv("https://drive.google.com/uc?id=10gZ-OIbxeylhxkHwxsar3D6FWj7c1qCg")
 #Select Movie
-title = st.text_input('Movie title', 'Type your film here !')
-st.write('The current movie title is', title)
+title = st.text_input('Choose your film', 'Type your film here !')
 df_MovieSelected = df_Movies[df_Movies["primaryTitle"].str.contains(title)]
 st.dataframe(df_MovieSelected["primaryTitle"])
 MovieSelectedTitle = st.selectbox('Choose your film ?', df_MovieSelected["primaryTitle"].tolist())
-st.write('You selected:', MovieSelectedTitle)
 df_MovieSelectedOne = df_Movies[df_Movies["primaryTitle"] == MovieSelectedTitle]
-st.dataframe(df_MovieSelectedOne)
+DisplayPoster(get_poster_from_api(df_MovieSelectedOne["tconst"]))
 
 # Define Side Menu ----------------------------------------------
 st.sidebar.title("Film Filters")
-st.write(df_MovieSelectedOne.actorsName)
-ActorList_list = st.sidebar.multiselect("Select Actor", df_MovieSelectedOne.actorsName)
-st.write(df_MovieSelectedOne.directorsName)
-DirectorList_list = st.sidebar.multiselect("Select Director", df_MovieSelectedOne.directorsName)
-st.write(df_MovieSelectedOne.genres)
-GenreList_list = st.sidebar.multiselect("Select Genre", df_MovieSelectedOne.genres)
+st.write(df_MovieSelectedOne["actorsName"].tolist())
+ActorList_list = st.sidebar.multiselect("Select Actor", df_MovieSelectedOne["actorsName"].tolist())
+DirectorList_list = st.sidebar.multiselect("Select Director", df_MovieSelectedOne["directorsName"].tolist())
+GenreList_list = st.sidebar.multiselect("Select Genre", df_MovieSelectedOne["genres"].tolist())
 
 df_Display = DisplayDataFrame(GenreList_list,DirectorList_list,ActorList_list)
 st.dataframe(df_Display)
