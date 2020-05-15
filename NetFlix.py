6d51a8f3fe05fe6df5b1c8d879e6ec1d45512354
roussetcedric <59645699+roussetcedric@users.noreply.github.com>
2020-05-15 11:56:14 +0200

    Update NetFlix.py

--- NetFlix.py
+++ NetFlix.py
@@ -62,7 +62,9 @@ title = st.text_input('Cherchez votre film', 'Taper un mot clé ici !')
 #st.dataframe(df_MovieSelected["primaryTitle"])
 df_SelectedNameAndYear = GetNameAndYear(df_Movies,title)
 MovieSelectedTitle = st.selectbox('Choississez votre film ?', df_SelectedNameAndYear["titleYear"].to_list())
-df_MovieSelectedOne = df_SelectedNameAndYear[df_SelectedNameAndYear["titleYear"] == MovieSelectedTitle]
+IndiceFilm = df_SelectedNameAndYear[df_SelectedNameAndYear["titleYear"] == MovieSelectedTitle]["tconst"]
+st.write(IndiceFilm)
+df_MovieSelectedOne = df_Movies[df_Movies["tconst"] == IndiceFilm]
 st.dataframe(df_MovieSelectedOne)
 DisplayPoster(get_poster_from_api(df_MovieSelectedOne.iloc[0]["tconst"]))
 # Define Side Menu ----------------------------------------------
