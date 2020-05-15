22e339585b3fc600a17b67d381ce310edf8f35d8
roussetcedric <59645699+roussetcedric@users.noreply.github.com>
2020-05-15 11:00:20 +0200

    Update NetFlix.py

--- NetFlix.py
+++ NetFlix.py
@@ -43,6 +43,13 @@ def get_poster_from_api(movie_id):
         poster_url = "https://raw.githubusercontent.com/roussetcedric/WCS_Public/master/pngtree-latest-movie-poster-design-image_163485.jpg"
     return poster_url
   
+def GetNameAndYear(dataFrameParam)
+    df_temp = dataFrameParam.loc[dataFrameParam['primaryTitle'].str.lower().str.contains(movie.value.lower())][['primaryTitle', 'startYear', 'tconst']].sort_values('startYear')
+    df_temp['titleYear'] = df_temp['primaryTitle'].map(str) + ' (' + df_temp['startYear'].map(str) + ')'
+    df_temp['movieTuple'] = list(zip(df_temp['titleYear'], df_temp['tconst']))
+    choices_list = df_temp['movieTuple'].to_list()
+    return choices_list
+  
 # Define Main Programm
 
 my_bar = st.progress(0)
@@ -54,7 +61,7 @@ for percent_complete in range(100):
 title = st.text_input('Cherchez votre film', 'Taper un mot clé ici !')
 df_MovieSelected = df_Movies[df_Movies["primaryTitle"].str.contains(title)]
 #st.dataframe(df_MovieSelected["primaryTitle"])
-MovieSelectedTitle = st.selectbox('Choississez votre film ?', df_MovieSelected["primaryTitle"].tolist())
+MovieSelectedTitle = st.selectbox('Choississez votre film ?', GetNameAndYear(df_MovieSelected))
 df_MovieSelectedOne = df_Movies[df_Movies["primaryTitle"] == MovieSelectedTitle]
 DisplayPoster(get_poster_from_api(df_MovieSelectedOne.iloc[0]["tconst"]))
 
